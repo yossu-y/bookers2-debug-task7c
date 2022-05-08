80918666f05b5121b62ae47ca989ffc20ba975ef
yossu-y <yangyuub@gmail.com>
2022-05-08 06:27:10 +0000

    [finish]グループ作成

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
 
   get "search" => "searches#search",as: "search"
 
+  resources :groups, except: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
